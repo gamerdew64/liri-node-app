88f370a3c63fe1c2c6b70bb6e77ec24b0a265a35
Joseph Perkins <gamerdew64@gmail.com>
2018-08-04 13:30:10 -0500

    adding default result code, updating comments

--- homework_instructions._updatedmd.md
+++ homework_instructions._updatedmd.md
@@ -1,7 +1,6 @@
-   * This will show your last 20 tweets and when they were created at in your terminal/bash window.
-   * If no song is provided then your program will default to "The Sign" by Ace of Base.
-   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-* In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-* Make sure you append each command you run to the `log.txt` file. 
 
-* The searches only work properly with one word responses. Is this the expected behavior?
+* If no song is provided then your program will default to "The Sign" by Ace of Base.
+* If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+* In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
+
+* rotten tomatoes score?
--- liri.js
+++ liri.js
@@ -2,10 +2,8 @@
 var keys = require('./keys.js');
 // fs is a core Node package for reading and writing files
 var fs = require("fs");
-
 // The following console.log was added to verify that the keys are showing in the console, and are being properly pulled
 // console.log(keys);
-
 // When Node invokes that require() function Node goes through the following sequence of steps:
   // Resolving: To find the absolute path of the file.
   // Loading: To determine the type of the file content.
@@ -24,15 +22,17 @@ var pullTweets = function() {
   // Creating a new variable that stores the keys from the keys file so that the tweets can get pulled
   var twitterUser = new Twitter(keys.twitter);
   // Assigning our twitter screen name/username to a new varible called "twitterHandle"
-  var twitterHandle = {screen_name: 'devUserAPI'};
+  var parameters = {
+    screen_name: 'devUserAPI',
+    count: 20
+  };
   // Loading the data from the server using the .get() request
-  twitterUser.get('statuses/user_timeline', twitterHandle, function(error, tweets, response) {
+  twitterUser.get('statuses/user_timeline', parameters, function(error, tweets, response) {
+    console.log("Searching for Tweets...");
     // If there are no errors, do the following:
     if (!error) {
-
       // The following console log line is for testing whether or not tweets show in the console.
       // console.log(tweets);
-
       // setting i to 0 and going through the length of the array, incrementing i each time
       for(var i=0; i<tweets.length; i++) {
         // Logging the twitter creation timestamp to the console
@@ -55,80 +55,135 @@ var pullArtist = function(artist) {
 var pullSpotify = function(songName) {
   // Searching to find the song name or "track"
   spotify.search({ type: 'track', query: songName }, function(err, data) {
-      if ( err ) {
-          console.log('Error occurred: ' + err);
-          return;
-      }
+    // Console logging the song name input by the user for testing
+    // console.log(songName);
+    console.log("Searching for Song...");
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    // Following two lines were for testing output
+    // console.log(data);
+    // console.log(data.tracks.items[0]);
+    // Creating a new variable that that contains the data.tracks.items information
+    var songs = data.tracks.items;
+    // setting i to 0 and going through the length of the array, incrementing i each time
+    for(var i=0; i<songs.length; i++){
+      // This console log adjusts the numbering in the output. Instead of counting 0 to 19, it counts 1 to 20
+      console.log(i+1);
+      // This line involves me using a method that we have not yet discussed.
+      // The map() method creates a new array with the results of calling a provided function on every element in the calling array.
+      // Logging the name of the artist
+      console.log('Artist(s): ' + songs[i].artists.map(pullArtist));
+      // Logging the song name
+      console.log('Song Name: ' + songs[i].name);
+      // Logging the preview of the song
+      console.log('Preview Song: ' + songs[i].preview_url);
+      // Logging the name of the album
+      console.log('Album ' + songs[i].album.name);
+      // Adding text to help divide the responses
+      console.log('\n##################################################\n');
+    } // Closing the for loop
 
-      // Following two lines were for testing output
-      // console.log(data);
-      // console.log(data.tracks.items[0]);
-
-      // Creating a new variable that that contains the data.tracks.items information
-      var songs = data.tracks.items;
-      // setting i to 0 and going through the length of the array, incrementing i each time
-      for(var i=0; i<songs.length; i++){
-        // This console log adjusts the numbering in the output. Instead of counting 0 to 19, it counts 1 to 20
-        console.log(i+1);
-        // This line involves me using a method that we have not yet discussed.
-        // The map() method creates a new array with the results of calling a provided function on every element in the calling array.
-        // Logging the name of the artist
-        console.log('artist(s): ' + songs[i].artists.map(pullArtist));
-        // Logging the song name
-        console.log('song name: ' + songs[i].name);
-        // Logging the preview of the song
-        console.log('preview song: ' + songs[i].preview_url);
-        // Logging the name of the album
-        console.log('album ' + songs[i].album.name);
-        // Adding text to help divide the responses
-        console.log('\n##################################################\n');
-      } // Closing the for loop
+    if (!songs) {
+      // Logging the name of the artist
+      console.log('Artist: ' + 'Ace of Base');
+      // Logging the song name
+      console.log('Song Name: ' + 'The Sign');
+      // Logging the preview of the song
+      console.log('Preview Song: ' + 'https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=6a090c0aa145423eb4e67f58c6067b50');
+      // Logging the name of the album
+      console.log('Album ' + 'The Sign (US Album) [Remastered]');
+      // Adding text to help divide the responses
+      console.log('\n##################################################\n');
+    } // Closing the if conditional
   }); // Closing spotify.search()
 }; // Closing the pullSpotify function
 
 var pullMovie = function(movieName){
   request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json&apikey=trilogy', function (error, response, body) {
+    // Console logging the move name input by the user for testing
+    // console.log(movieName);
+    console.log("Searching for Movies...");
     // Print the error if one occurred
     console.log('error:', error);
     // Print the response status code if a response was received
     console.log('statusCode:', response && response.statusCode);
     // console.log('body:', body); // Print the HTML for the above URL.
     var jsonData = JSON.parse(body);
-
     // Including to test what all is included in the body of the OMBI API
-    // console.log("#######################################\n" + "#######################################\n" + "#######################################\n" + body + "#######################################\n" + "#######################################\n" + "#######################################\n");
-
-    // Logging the name of the movie to the console
-    console.log("#######################################\n" + 'Title: ' + jsonData.Title);
-    // Logging the year the movie was released to the console
-    console.log('Year: ' + jsonData.Year);
-    // Logging the rating (MPAA) of the movie to the console
-    console.log('Rated: ' + jsonData.Rated);
-    // Logging the runtime of the movie to the console
-    console.log('Runtime: ' + jsonData.Runtime);
-    // Logging the IMDB rating of the movie to the console
-    console.log('IMDB Rating: ' + jsonData.imdbRating);
-    // Logging the number of rating votes of the movie from IMDB to the console
-    console.log('IMDB Rating Vote Number: ' + jsonData.imdbVotes);
-    // Logging the country where the movie was released to the console
-    console.log('Country: ' + jsonData.Country);
-    // Logging the language of the movie to the console
-    console.log('Language: ' + jsonData.Language);
-    // Logging the plot of the movie to the console
-    console.log('Plot: ' + jsonData.Plot);
-    // Logging the actors of the movie to the console
-    console.log('Actors: ' + jsonData.Actors);
-    // Logging the metacritic score of the movie to the console
-    console.log('Metacritic Score: ' + jsonData.Metascore + "\n#######################################");
-    // The following two lines were originally supposed to be the Rotten tomatoes rating and URL, but they don't show up in the object, so I instead used the metacritic score and website above
-    // console.log('Rotten Tomatoes Rating: ' + jsonData.tomatoRating);
-    // console.log('Rotten Tomatoes URL: ' + jsonData.tomatoURL + "\n#######################################");
-  });
-};
+    // console.log("#######################################\n" + body + "#######################################\n");
+    // If the user entered the movie-this command, but DID NOT enter a move name, run the following:
+    if (!movieName) {
+      console.log("#######################################\n" + "You did not enter the name of a movie. Please see the following information regarding Mr. Peabody.");
+      console.log("If you have not watched Mr. Nobody, then you should. It is on Netflix!!!");
+      // Then run a request to the OMDB API with the movie specified
+      var noMovieProvided = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+      // This line is just to help us debug against the actual URL - for testing purposes
+      // console.log(noMovieProvided);
+      request(noMovieProvided, function(error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+          // Logging the name of the movie to the console
+          console.log("#######################################\n" + 'Title: ' + JSON.parse(body).Title);
+          // Logging the year the movie was released to the console
+          console.log("Release Year: " + JSON.parse(body).Year);
+          // Logging the rating (MPAA) of the movie to the console
+          console.log('Rated: ' + JSON.parse(body).Rated);
+          // Logging the runtime of the movie to the console
+          console.log('Runtime: ' + JSON.parse(body).Runtime);
+          // Logging the IMDB rating of the movie to the console
+          console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
+          // Logging the number of rating votes of the movie from IMDB to the console
+          console.log('IMDB Rating Vote Number: ' + JSON.parse(body).imdbVotes);
+          // Logging the country where the movie was released to the console
+          console.log('Country: ' + JSON.parse(body).Country);
+          // Logging the language of the movie to the console
+          console.log('Language: ' + JSON.parse(body).Language);
+          // Logging the plot of the movie to the console
+          console.log('Plot: ' + JSON.parse(body).Plot);
+          // Logging the actors of the movie to the console
+          console.log("Actors: " + JSON.parse(body).Actors);
+          // Logging the metacritic score of the movie to the console
+          console.log('Metacritic Score: ' + JSON.parse(body).Metascore + "\n#######################################");
+        } // Closing the above if statement - (!error && response.statusCode === 200)
+      }); // Closing the request when no movie is provided
+    } // Closing the if statement that sees if a move name has been entered - (!movieName)
+    // If the user DID enter a move name, run the following:
+    else {
+      // Logging the name of the movie to the console
+      console.log("#######################################\n" + 'Title: ' + jsonData.Title);
+      // Logging the year the movie was released to the console
+      console.log('Year: ' + jsonData.Year);
+      // Logging the rating (MPAA) of the movie to the console
+      console.log('Rated: ' + jsonData.Rated);
+      // Logging the runtime of the movie to the console
+      console.log('Runtime: ' + jsonData.Runtime);
+      // Logging the IMDB rating of the movie to the console
+      console.log('IMDB Rating: ' + jsonData.imdbRating);
+      // Logging the number of rating votes of the movie from IMDB to the console
+      console.log('IMDB Rating Vote Number: ' + jsonData.imdbVotes);
+      // Logging the country where the movie was released to the console
+      console.log('Country: ' + jsonData.Country);
+      // Logging the language of the movie to the console
+      console.log('Language: ' + jsonData.Language);
+      // Logging the plot of the movie to the console
+      console.log('Plot: ' + jsonData.Plot);
+      // Logging the actors of the movie to the console
+      console.log('Actors: ' + jsonData.Actors);
+      // Logging the metacritic score of the movie to the console
+      console.log('Metacritic Score: ' + jsonData.Metascore + "\n#######################################");
+      // The following two lines were originally supposed to be the Rotten tomatoes rating and URL, but they don't show up in the object, so I instead used the metacritic score and website above
+      // console.log('Rotten Tomatoes Rating: ' + jsonData.tomatoRating);
+      // console.log('Rotten Tomatoes URL: ' + jsonData.tomatoURL + "\n#######################################");
+    } // Closing else statement
+  }); // Closing the first request (that pipes in the movie name enterd by the user)
+}; // Closing the pullMovie function
 
 var runRandom = function(){
   // Read the random text file
   fs.readFile('random.txt', 'utf8', function (err, data) {
+    console.log("Searching through random.txt...");
     if (err) throw err;
     // The next line is done for testing purposes
     // console.log(data);
@@ -162,13 +217,23 @@ var userChoice = function(caseData, functionData) {
       break;
     default:
     console.log('LIRI does not understand...');
-  }
-};
+  } // Closing the switch statement
+}; // Closing the userChoice function
 
 // Creating a runThis function that takes in two arguments which, when called will be the two user inputs after typing node liri.js in the console
-var runThis = function(argOne, argTwo) {
-  userChoice(argOne, argTwo);
+var runThis = function(firstArgument, secondArgument) {
+  userChoice(firstArgument, secondArgument);
 };
 
-// Calling the runThis function, and passing in the two user inputs (process.argv for indexes 2 and 3) into argOne and argTwo
-runThis(process.argv[2], process.argv[3]);
+// Setting a new variable, called userInput to an empty string to make it easier to pull the data that is inputted from the user
+var userInput = "";
+
+// Capture all the words in the address (again ignoring the first three Node arguments, since the third one is the command that determines which function will be run)
+for (var i = 3; i < process.argv.length; i++) {
+
+  // Build a string with the userInput.
+  userInput = userInput + " " + process.argv[i];
+}
+
+// Calling the runThis fucntion using the first user input (which they choose if they want tweets, movies, music, random txt) and the specfic thing which the user wants to search (userInput)
+runThis(process.argv[2], userInput);
